69351658a3c1f5419bcba010be81c9b9fdee91c1
尼古拉斯常 <1119577587@qq.com>
2023-09-22 12:23:38 +0800

    地址生成头像

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,13 @@ import { useClientLoaderData, setLocale} from 'umi';
 import { useAccount, useDisconnect } from 'wagmi';
 import useTranslationLanguage from '@/hooks/useTranslationLanguage';
 import './index.less'
+import multiavatar from '@multiavatar/multiavatar'
+
 export default function HomePage() {
+
+  const svgCode = multiavatar('abc')
+  console.log('svgCode==',svgCode)
+
 
   // const info = useWeb3React()
   // console.log('info===',info)
@@ -31,6 +37,11 @@ export default function HomePage() {
 
   return (
     <div style={{color:'red'}}>
+
+      <div style={{width:100,height:100}} dangerouslySetInnerHTML={{__html:svgCode}}>
+
+      </div>
+
       <button onClick={onClick}>{address || '链接钱包'}</button>
       <div>{t('welcome')}</div>
       <div>{t('welcome1',{name:'哈哈'})}</div>
